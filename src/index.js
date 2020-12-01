e149a77d1709824c495b2206e57306e3d63f7ad2
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-12-01 10:58:26 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+  return (<View>
+    <Text>
+      Fala Galera!!!
+    </Text>
+  </View>);
+};
