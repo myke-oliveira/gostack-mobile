f3bd8f0db294f0a5ab5e79bb29ab3911e20773ff
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-12-01 12:02:56 -0300

    List projects with scrowview

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, Text, StyleSheet, StatusBar } from "react-native";
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
   return (<>
     <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Fala Galera!!!
-      </Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {projects.map(project =>
+      <Text style={styles.title} key={projects.id}>
+        {project.title}
+      </Text>)}
+    </ScrollView>
   </>);
 };
 
@@ -16,13 +28,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
   },
 
   title: {
     color: '#fff',
-    fontSize: 32,
+    fontSize: 120,
     fontWeight: 'bold',
   }
 });
